6b4b7197a483b1aa85453ee3723f47bb0defe712
Pedro Santiago <pedrossantiago1@gmail.com>
2023-02-13 15:22:20 -0300

    esqueleto para a aplicacao

---
+++ src/components/mainMenu.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { fly } from 'svelte/transition'
+</script>
+
+<div
+	class="ml-4 -mt-2 h-fit w-fit rounded-md border border-neutral-600 bg-neutral-800 px-3 py-1"
+	transition:fly={{ y: -10, duration: 100 }}
+>
+	<ul class="space-y-2 text-white">
+		<li>FOda1</li>
+		<li>FOda1</li>
+		<li>FOda1</li>
+		<li>FOda1</li>
+		<li>FOda1</li>
+		<li>FOda1</li>
+	</ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import MainMenu from '../components/mainMenu.svelte'
+	let dropdown = false
+</script>
+
+<div class="h-screen bg-neutral-900">
+	<div class="flex justify-between p-4">
+		<button
+			class="rounded-md border border-neutral-600 bg-neutral-800 p-2"
+			on:click={() => (dropdown = !dropdown)}
+		>
+			<img
+				src="/src/icons/hamburger.svg"
+				alt="menu icon"
+				class=" h-6 w-6 invert"
+			/>
+		</button>
+		<div class="mx-auto w-80 rounded-md bg-green-500" />
+	</div>
+
+	{#if dropdown}
+		<MainMenu />
+	{/if}
+</div>
